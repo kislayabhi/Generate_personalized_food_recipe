6c8f0e021aedf295c37863829ec8d25a3d71219c
Abhijeet Kislay <abhijeetkislay@Jinghaos-MBP.localdomain>
2024-04-26 17:29:18 -0400

    code cleanups

--- stremlit_llm.py
+++ stremlit_llm.py
@@ -1,13 +1,14 @@
-# Bring in deps
+# Import dependencies
 from api_key import llm_key
 from trubrics.integrations.streamlit import FeedbackCollector
-
 import streamlit as st
 import google.generativeai as genai
 
-# App framework
+# Set up Streamlit app
 st.title('Food Recipe Gemini Generator')
 st.subheader('Get Food Recipe based on Grocery, Time Required, Cuisines and Equipment available (all inputs are optional)')
+
+# User inputs
 api_key =  st.text_input('Enter Google Generative AI API KEY (Required)')
 st.link_button("Click for API KEY (select create api key in new project)", "https://makersuite.google.com/app/apikey", type="secondary")
 food = st.text_input('Particular Food in Mind (Dal Tadka, cake)')
@@ -23,6 +24,7 @@ extra = st.text_input('Additional information/requests')
 # Prompt templates
 prompt_enter = st.button("Recipe") 
 
+# Construct the prompt
 prompt = f''' Can you create a concise healthy food recipie (40 lines max) avoiding allergies, based on food in mind, groceries I have, time needed, meal, cuisine, equipment present and extra info:
 food in mind:{food},
 grocery:{grocery} ,
@@ -55,32 +57,28 @@ use the following format in seperate lines
         .
 '''
 
-# Llms
+# Set up Generative AI
 llm_api_key = api_key if api_key else llm_key
 genai.configure(api_key=llm_api_key)
 model = genai.GenerativeModel(model_name = "gemini-pro")
 
+# Add this line where you want the file uploader to appear
+uploaded_file = st.file_uploader("Choose a file")
+
+# Check if a file has been uploaded
+if uploaded_file is not None:
+    # Do something with the file
+    file_contents = uploaded_file.read()
+    st.write(file_contents)
+
+# Footer
 st.write("Made with ❤️ by [Abhijeet Kislay](https://kislayabhi.github.io/projects/) ©️ 2024 _||_[linkedin](https://www.linkedin.com/in/abhijeetkislay/)")
 
-
-#collector = FeedbackCollector(
-#    project="default",
-#    email=st.secrets["TRUBRICS_EMAIL"],
-#    password=st.secrets["TRUBRICS_PASSWORD"],
-#)
-
-
-# Show stuff to the screen if there's a prompt
+# Generate content if there's a prompt
 try:
     if prompt_enter:
         response = model.generate_content(prompt)
         st.write(response.text)
         st.write("Please leave feedback")
-        # collector.st_feedback(
-        #     component="default",
-        #     feedback_type="thumbs",
-        #     open_feedback_label="[Optional] Provide additional feedback",
-        #     model="gemeni",
-        #     prompt_id=prompt,)
 except Exception as error:
     st.write("Please check your Api key, probable issue", SystemExit(error))
